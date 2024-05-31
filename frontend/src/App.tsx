ad39779231637362aa6b22bdb3068a6dcee400af
ns29248 <Nazif.selimi73@gmail.com>
2024-05-31 20:53:05 +0200

    Re-add frontend folder

---
+++ frontend/src/App.tsx
@@ -0,0 +1,63 @@
+import { ChangeEvent, SyntheticEvent, useState } from "react";
+import "./App.css";
+import CardList from "./Components/CardList/CardList";
+import Search from "./Components/Search/Search";
+import { searchCompanies } from "./api";
+import { CompanySearch } from "./company";
+import ListPortfolio from "./Components/Portfolio/ListPortfolio/ListPortfolio";
+
+function App() {
+  const [search, setSearch] = useState<string>("");
+  const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
+  const [portfolioValues, setPortfolioValues] = useState<string[]>([]);
+  const [serverError, setServerError] = useState<string>("");
+
+  const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+  const onPortfolioCreate = (e: any) => {
+    e.preventDefault();
+    const exitst = portfolioValues.find((value) => value === e.target[0].value);
+    if (exitst) return;
+    const updatedPortfolio = [...portfolioValues, e.target[0].value];
+    setPortfolioValues(updatedPortfolio);
+  };
+  const onPortfolioDelete = (e: any) => {
+    e.preventDefault();
+    const removed = portfolioValues.filter((value) => {
+      return value !== e.target[0].value;
+    });
+    setPortfolioValues(removed);
+  };
+
+  const onSearchSubmit = async (e: SyntheticEvent) => {
+    e.preventDefault();
+    const result = await searchCompanies(search);
+    if (typeof result === "string") {
+      setServerError(result);
+    } else if (Array.isArray(result.data)) {
+      setSearchResult(result.data);
+    }
+    console.log(searchResult);
+  };
+  return (
+    <div className="App">
+      {serverError && <h1>{serverError}</h1>}
+      <Search
+        onSearchSubmit={onSearchSubmit}
+        search={search}
+        handleSearchChange={handleSearchChange}
+      />
+      <ListPortfolio
+        portfolioValues={portfolioValues}
+        onPortfolioDelete={onPortfolioDelete}
+      />
+      <CardList
+        searchResults={searchResult}
+        onPortfolioCreate={onPortfolioCreate}
+      />
+    </div>
+  );
+}
+
+export default App;
