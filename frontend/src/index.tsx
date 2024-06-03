887b8cdd3d133973805c046c167cef9a331464d1
ns29248 <Nazif.selimi73@gmail.com>
2024-06-03 21:08:41 +0200

    Setting up react-router and react-router-dom

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,14 +4,16 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { searchCompanies } from "./api";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./Routes/Routes";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-console.log(searchCompanies("APPL"));
+// console.log(searchCompanies("APPL"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
